e24e36d3684728330ca76c871b84780e2e4a010b
Chase <chasedemaster.tech@gmail.com>
2022-06-28 06:09:28 -0500

    commit tree 0974a65f3bd17f768e6b4214c16461c97b0f1190

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    About
+    <h2>
+      About Me
+    </h2>
+    <p>
+      Pro Choice
+    </p>
+    <img src={image} alt="I made this" />
+  </div>
+  )
 }
 
 export default About;
